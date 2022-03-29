df9db8987fabdebbdfb17707a116eecc66547b23
Quan. Hoang Do - CMC Global DJ2 <hdquan@cmcglobal.vn>
2022-03-29 16:12:43 +0700

    About state

--- src/views/Example/MyComponents.js
+++ src/views/Example/MyComponents.js
@@ -9,12 +9,11 @@ class MyComponents extends React.Component {
 
     render() {
 
-        let name = 'Hoang Quan';
-
         return (
             <>
                 <div className="Hello">hello </div>
                 <div className="name">{this.state.name}</div>
+                <div className="channel">{this.state.channel}</div>
             </>
         );
     }
