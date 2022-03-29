0294f154aee71370ab95c46ea5bc87bb3242c67a
Quan. Hoang Do - CMC Global DJ2 <hdquan@cmcglobal.vn>
2022-03-29 15:38:17 +0700

    hello world!

---
+++ src/views/Example/MyComponents.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MyComponents extends React.Component {
+
+    render() {
+        return (
+            <div>hello my component</div>
+        );
+    }
+}
+
+export default MyComponents;
