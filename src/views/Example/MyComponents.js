b583f79674991b94bedb8147325ca20465c1a637
Quan. Hoang Do - CMC Global DJ2 <hdquan@cmcglobal.vn>
2022-03-29 16:03:45 +0700

    Demo React Fragment Hello name

--- src/views/Example/MyComponents.js
+++ src/views/Example/MyComponents.js
@@ -2,9 +2,20 @@ import React from "react";
 
 class MyComponents extends React.Component {
 
+    state = {
+        name: 'Hoang Quan',
+        channel: 'facebook.com/hoangquan1811'
+    }
+
     render() {
+
+        let name = 'Hoang Quan';
+
         return (
-            <div>hello my component</div>
+            <>
+                <div className="Hello">hello </div>
+                <div className="name">{this.state.name}</div>
+            </>
         );
     }
 }
